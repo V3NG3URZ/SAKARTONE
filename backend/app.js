207dfe825a4974de77e2d85e669dd9ed7c41ea4d
V3NG3URZ <84502806+V3NG3URZ@users.noreply.github.com>
2022-06-03 10:44:59 +0200

    test

---
+++ backend/app.js
@@ -0,0 +1,35 @@
+const express = require('express');
+const mongoose = require('mongoose');
+
+// A MODIFIER AVEC LES INFOS DE VOTRE CONNEXION 
+mongoose.connect('mongodb://localhost:27017/formapp',
+  { useNewUrlParser: true,
+    useUnifiedTopology: true })
+  .then(() => console.log('Connexion à MongoDB réussie !'))
+  .catch(() => console.log('Connexion à MongoDB échouée !'));
+
+
+const userRoutes = require('./routes/user');
+
+const app = express();
+
+// Middleware
+app.use(express.json()); // Pour extraire le corps JSON des requêtes POST
+
+app.use((req, res, next) => {
+  res.setHeader('Access-Control-Allow-Origin', '*');
+  res.setHeader('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content, Accept, Content-Type, Authorization');
+  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, PATCH, OPTIONS');
+  next();
+});
+
+// Routes
+app.use('/api/users', userRoutes);
+app.get('/', (req, res, next) => {
+  res.statusCode=200;
+  res.setHeader('Content-Type', 'text/plain');
+  res.end('Hello World');
+});
+
+
+module.exports = app;
